872d9e7f416f320bfdd3abec0e10e099e2f7d931
kyoungjin <soccer910@naver.com>
2026-01-29 15:21:23 +0900

    js project commit

---
+++ src/App.js
@@ -0,0 +1,209 @@
+import Header from "./components/Header.js";
+import RegionList from "./components/RegionList.js";
+import CityList from "./components/CityList.js";
+import CityDetail from "./components/CityDetail.js";
+
+import { request, requestCityDetail } from "./components/api.js";
+
+export default function App($app) {
+  const getSortBy = () => {
+    if (window.location.search) {
+      return window.location.search.split("sort=")[1].split("&")[0];
+    }
+    return "total";
+  };
+
+  const getSearchWord = () => {
+    if (window.location.search && window.location.search.includes("search=")) {
+      return window.location.search.split("search=")[1];
+    }
+    return "";
+  };
+
+  this.state = {
+    startIdx: 0,
+    sortBy: getSortBy(),
+    searchWord: getSearchWord(),
+    region: "",
+    cities: "",
+    currentPage: window.location.pathname,
+  };
+
+  const renderHeader = () => {
+    new Header({
+      $app,
+      initialState: {
+        sortBy: this.state.sortBy,
+        searchWord: this.state.searchWord,
+        currentPage: this.state.currentPage,
+      },
+      handleSortChange: async (sortBy) => {
+        const pageUrl = `/${this.state.region}?sort=${sortBy}`;
+        history.pushState(
+          null,
+          null,
+          this.state.searchWord
+            ? pageUrl + `&search=${this.state.searchWord}`
+            : pageUrl,
+        );
+        const cities = await request(
+          0,
+          this.state.region,
+          sortBy,
+          this.state.searchWord,
+        );
+        this.setState({
+          ...this.state,
+          startIdx: 0,
+          sortBy: sortBy,
+          cities: cities,
+        });
+      },
+      handleSearchWord: async (searchWord) => {
+        history.pushState(
+          null,
+          null,
+          `/${this.state.region}?sort=${this.state.sortBy}&search=${searchWord}`,
+        );
+        const cities = await request(
+          0,
+          this.state.region,
+          this.state.sortBy,
+          searchWord,
+        );
+        this.setState({
+          ...this.state,
+          startIdx: 0,
+          searchWord: searchWord,
+          cities: cities,
+        });
+      },
+    });
+  };
+
+  const renderRegionList = () => {
+    new RegionList({
+      $app,
+      initialState: this.state.region,
+      handleRegion: async (region) => {
+        history.pushState(null, null, `/${region}?sort=total`);
+        const cities = await request(0, region, "total");
+        this.setState({
+          ...this.state,
+          startIdx: 0,
+          region: region,
+          sortBy: "total",
+          searchWord: "",
+          cities: cities,
+        });
+      },
+    });
+  };
+
+  const renderCityList = () => {
+    new CityList({
+      $app,
+      initialState: this.state.cities,
+      handleLoadMore: async () => {
+        const newStartIdx = this.state.startIdx + 40;
+        const newCities = await request(
+          newStartIdx,
+          this.state.region,
+          this.state.sortBy,
+          this.state.searchWord,
+        );
+
+        this.setState({
+          ...this.state,
+          startIdx: newStartIdx,
+          cities: {
+            cities: [...this.state.cities.cities, ...newCities.cities],
+            isEnd: newCities.isEnd,
+          },
+        });
+      },
+      handleItemClick: (id) => {
+        history.pushState(null, null, `/city/${id}`);
+        this.setState({
+          ...this.state,
+          currentPage: `/city/${id}`,
+        });
+      },
+    });
+  };
+
+  const renderCityDetail = async (cityId) => {
+    try {
+      const cityDetailData = await requestCityDetail(cityId);
+      new CityDetail({ $app, initialState: cityDetailData });
+    } catch (error) {
+      console.error("도시 상세 정보 요청 중 오류 발생:", eroor);
+    }
+  };
+
+  const render = () => {
+    const path = this.state.currentPage;
+    $app.innerHTML = "";
+    if (path.startsWith("/city/")) {
+      const cityId = path.split("/city/")[1];
+      renderHeader();
+      renderCityDetail(cityId);
+    } else {
+      renderHeader();
+      renderRegionList();
+      renderCityList();
+    }
+  };
+
+  this.setState = (newState) => {
+    this.state = newState;
+    render();
+  };
+
+  // Handle browser navigation (back/forward)
+  window.addEventListener("popstate", async () => {
+    const urlPath = window.location.pathname;
+
+    const prevRegion = urlPath.replace("/", "");
+    const prevSortBy = getSortBy();
+    const prevSearchWord = getSearchWord();
+    const prevStartIdx = 0;
+    const prevCities = await request(
+      prevStartIdx,
+      prevRegion,
+      prevSortBy,
+      prevSearchWord,
+    );
+
+    this.setState({
+      ...this.state,
+      startIdx: prevStartIdx,
+      sortBy: prevSortBy,
+      searchWord: prevSearchWord,
+      region: prevRegion,
+      cities: prevCities,
+      currentPage: urlPath,
+    });
+  });
+
+  const init = async () => {
+    const path = this.state.currentPage;
+
+    if (path.startsWith("/city/")) {
+      render();
+    } else {
+      const cities = await request(
+        this.state.startIdx,
+        this.state.region,
+        this.state.sortBy,
+        this.state.searchWord,
+      );
+      this.setState({
+        ...this.state,
+        cities: cities,
+      });
+    }
+  };
+
+  init();
+}
---
+++ src/components/api.js
@@ -0,0 +1,38 @@
+const API_URL = "https://trip-wiki-api.vercel.app/";
+
+export const request = async (startIdx, region, sortBy, searchWord) => {
+  try {
+    let url = `${API_URL}`;
+    if (region && region !== "All") {
+      url += `${region}?start=${startIdx}`;
+    } else {
+      url += `?start=${startIdx}`;
+    }
+    if (sortBy) {
+      url += `&sort=${sortBy}`;
+    }
+    if (searchWord) {
+      url += `&search=${searchWord}`;
+    }
+
+    const response = await fetch(url);
+    if (response) {
+      let data = await response.json();
+      return data;
+    }
+  } catch (error) {
+    console.error("API 요청 중 오류 발생:", error);
+  }
+};
+
+export const requestCityDetail = async (cityId) => {
+  try {
+    const response = await fetch(`${API_URL}city/${cityId}`);
+    if (response) {
+      let data = await response.json();
+      return data;
+    }
+  } catch (error) {
+    console.error("API 요청 중 오류 발생:", error);
+  }
+};
